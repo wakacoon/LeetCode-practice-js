8a03e22ff4fb6eee3c2eca21614eccd4d513af77
wakacoon <wy1021487518@163.com>
2022-01-02 15:16:08 +0800

    fix

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -12,9 +12,13 @@
  */
 var twoSum = function(nums, target) {
     let list = [9,4,7,3,8,5,2,1];
-    Bubble(list);
+    // Bubble(list);
+    BubbleFix(list);
 };
 
+/**
+ * 冒泡排序
+ */
 function Bubble (nums){
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
@@ -26,6 +30,62 @@ function Bubble (nums){
         }
     }
     console.log(nums);
+}
+
+/**
+ * 冒泡优化
+ */
+ function BubbleFix (nums){
+    for (let i = 0; i < nums.length; i++) {
+        let resort = false;
+        for (let j = i+1; j < nums.length; j++) {
+            if(nums[i] > nums[j]){
+                let pivot = nums[i];
+                nums[i] = nums[j];
+                nums[j] = pivot;
+                resort = true;
+            }
+        }
+        if (!resort){
+            break;
+        }
+    }
+    
+    console.log(nums);
+}
+
+/**
+ * 鸡尾酒法
+ */
+
+ function BubbleFix (nums){
+    for (let i = 0; i < Floor(nums.length/2); i++) {
+        let resort = false;
+        let length = nums.length;
+        for (let j = i+1; j < nums.length; j++) {
+            if(nums[i] > nums[j]){
+                let pivot = nums[i];
+                nums[i] = nums[j];
+                nums[j] = pivot;
+                resort = true;
+            }
+        }
+        if (!resort){
+            break;
+        }
+        for (let k = length - i -1; k > i; k--) {
+            if(nums[length - i] < nums[k]){
+                let pivot = nums[length - i];
+                nums[length - i] = nums[k];
+                nums[k] = pivot;
+                resort = true;
+            }
+            
+        }
+    }
+    
+    console.log(nums);
+    console.log('----------');
 }
 
 // @lc code=end
